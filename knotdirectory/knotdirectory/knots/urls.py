c60801bee95b92857c6a3694934047c8aeff4974
Justin Steward <justin@justinsteward.com>
2013-08-18 19:43:07 +1000

    sort by tags

--- knotdirectory/knotdirectory/knots/urls.py
+++ knotdirectory/knotdirectory/knots/urls.py
@@ -4,7 +4,8 @@ from .views import KnotListView, KnotDetailView
 
 urlpatterns = patterns(
     '',
-    url(r'^knots/', KnotListView.as_view(), name="knots.list"),
+    url(r'^knots/$', KnotListView.as_view(), name="knots.list"),
+    url(r'^knots/(?P<tag>.+)/', KnotListView.as_view(), name="knots.list"),
     url(r'^knot/(?P<pk>\d+)/', KnotDetailView.as_view(), name="knots.detail"),
 
 )
--- knotdirectory/knotdirectory/knots/views.py
+++ knotdirectory/knotdirectory/knots/views.py
@@ -7,6 +7,13 @@ class KnotListView(ListView):
     model = Knot
     paginate_by = 40
 
+    def get_queryset(self):
+        queryset = super(KnotListView, self).get_queryset()
+        tag = self.kwargs.get('tag')
+        if tag:
+            queryset = queryset.filter(tags__name__in=[tag])
+        return queryset
+
 
 class KnotDetailView(DetailView):
     model = Knot
