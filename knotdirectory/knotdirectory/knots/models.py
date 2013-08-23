1182aa5fe4e5681bd19f405a2f1924d1bd46cf5c
Justin Steward <justin@justinsteward.com>
2013-08-23 12:59:54 +1000

    add foriegn key from knots to creators, automate creation of creator

--- knotdirectory/knotdirectory/knots/models.py
+++ knotdirectory/knotdirectory/knots/models.py
@@ -12,12 +12,23 @@ User = get_user_model()
 class Knot(models.Model):
     name = models.CharField(max_length=90, help_text="Commonly accepted name for this tie")
     other_names = models.TextField(help_text="Is this knot known by other names? One name per line, please", blank=True)
-    creator = models.CharField(max_length=90, help_text="Who should we credit for discovering this tie")
+    creator_name = models.CharField(max_length=90, help_text="Who should we credit for discovering this tie")
+    creator = models.ForeignKey('CreatorProfile', blank=True, null=True)
     notes = models.TextField(help_text="Any other information? Markdown text enabled.", blank=True)
     tags = TaggableManager()
     date_added = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to="knotsimages/%Y/%m/", help_text="A photo of the completed tie.", null=True, blank=True)
+
+    def save(self):
+        if not self.creator:
+            try:
+                self.creator = CreatorProfile.objects.get(name=self.creator_name)
+            except CreatorProfile.DoesNotExist:
+                cp = CreatorProfile()
+                cp.name = self.creator_name
+                cp.save()
+        super(Knot, self).save()
 
     def get_absolute_url(self):
         return reverse("knots.detail", args=[self.pk, ])
