ed1172cc12654cdf0d221281cd56c51992b4c4af
Justin Steward <justin@justinsteward.com>
2013-09-05 13:04:24 +1000

    Set up form to use special widget for tags

--- knotdirectory/knotdirectory/knots/forms.py
+++ knotdirectory/knotdirectory/knots/forms.py
@@ -1,10 +1,41 @@
 from django import forms
 from django.forms.models import inlineformset_factory
+from django.utils import six
 
 from .models import Knot, Link
 
 
+def edit_string_for_tags(tags):
+    """
+    Convert list of Tags into comma-separated list.
+
+    We can't use the one built into django-taggit, because select2 freaks out
+    about the space after the comma.
+    """
+    names = []
+    for tag in tags:
+        name = tag.name
+        if ',' in name or ' ' in name:
+            names.append('"%s"' % name)
+        else:
+            names.append(name)
+    return ','.join(sorted(names))
+
+
+class HiddenTagWidget(forms.HiddenInput):
+    """ For use with select2 and ajax tagging """
+    def render(self, name, value, attrs=None):
+        if value is not None and not isinstance(value, six.string_types):
+            value = edit_string_for_tags([o.tag for o in value.select_related("tag")])
+        return super(HiddenTagWidget, self).render(name, value, attrs)
+
+
 class KnotForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(KnotForm, self).__init__(*args, **kwargs)
+        print self.fields.keys()
+        self.fields['tags'].widget = HiddenTagWidget()
+
     class Meta:
         model = Knot
         fields = ['name', 'photo', 'other_names', 'creator_name', 'notes', 'tags']
